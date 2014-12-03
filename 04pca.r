c38e57d94e347f97da3e280e95fa97c2cb5ad26b
Hidemasa Bono <bonohu@gmail.com>
2014-12-03 17:29:21 +0900

    column fix. again.

--- 04pca.r
+++ 04pca.r
@@ -4,4 +4,4 @@ names(data.pca)
 plot(data.pca$sdev, type="h", main="PCA s.d.")
 data.pca.sample <- t(data) %*% data.pca$rotation[,1:2]
 plot(data.pca.sample, main="PCA")  
-text(data.pca.sample, colnames(data), col = c(rep("red", 4), rep("blue",4),rep("green",4)))
+text(data.pca.sample, colnames(data), col = c(rep("red", 3), rep("blue",3),rep("green",3),rep("black",3)))
