07b33864e9ab2ca374007a60abbab2449d521ae0
Hidemasa Bono <bonohu@gmail.com>
2014-12-03 10:18:33 +0900

    Variable GO -> Study

--- 01piechart.r
+++ 01piechart.r
@@ -3,5 +3,5 @@ dat <- read.delim2("srabystudy.txt", header=F)
 names(dat) <- c("Study", "freq")
 dat <- cbind(dat, serial=seq(dim(dat)[1]))
 dat2 <- dat[ dat$freq != 0, ]
-pie(dat2$freq, labels=paste(dat2$GO, dat2$freq), col=rainbow(dim(dat[1])[dat2$serial]), main="SRA by Study")
+pie(dat2$freq, labels=paste(dat2$Study, dat2$freq), col=rainbow(dim(dat[1])[dat2$serial]), main="SRA by Study")
 dev.off()
